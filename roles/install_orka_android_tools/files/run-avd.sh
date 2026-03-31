d926b9f294114daf554cae9172ca468cd66cf90d
Spike Burton <sburton@macstadium.com>
2026-03-31 16:34:04 -0400

    add emulator options for audio, cpu, memory

--- roles/install_orka_android_tools/files/run-avd.sh
+++ roles/install_orka_android_tools/files/run-avd.sh
@@ -4,27 +4,44 @@ set -eu -o pipefail
 
 CONSOLE_PORT="5554"
 VMNET_BRIDGE_IP="192.168.64.1"
+RELAY_PORT=""
+
 VERBOSE=0
 
-RELAY_PORT=""
+AUDIO_FLAG="-no-audio"
+CPU_FLAG=""
+MEMORY_FLAG=""
 
 declare -a ARGS
 
 usage () {
-  echo "Usage: $0 AVD_NAME -b <VMNET_BRIDGE_IP> -p <CONSOLE_PORT> -r <RELAY_PORT>"
+  echo "Usage: $0 AVD_NAME [FLAGS] [-v]"
+  echo "Options:"
+  echo " -a Enable audio"
+  echo " -b <opt> IP address for Orka vmnet bridge interface"
+  echo " -c <opt> CPU cores"
+  echo " -m <opt> Memory size in MBs"
+  echo " -p <opt> Console port"
+  echo " -r <opt> Relay port accessible from Orka VM via vmnet bridge interface"
+  echo " -v Enable verbose mode"
+  echo " -h Print usage and exit"
   exit 1
 }
 
 while [[ $# -gt 0 ]]; do
   unset OPTIND
   unset OPTARG
-  while getopts "b:p:r:hv" opt; do
+  while getopts "ab:c:m:p:r:hv" opt; do
     case $opt in
+      a) AUDIO_FLAG="" ;;
       b) VMNET_BRIDGE_IP="$OPTARG" ;;
+      c) CPU_FLAG="-cores $OPTARG" ;;
+      m) MEMORY_FLAG="-memory $OPTARG" ;;
       p) CONSOLE_PORT="$OPTARG" ;;
       r) RELAY_PORT="$OPTARG" ;;
-      h) usage ;;
       v) VERBOSE=1 ;;
+      h) usage ;;
+      \?) usage ;;
     esac
   done
   shift $(($OPTIND - 1))
@@ -59,9 +76,9 @@ cleanup () {
   wait $EMULATOR_PID $RELAY_PID 2>/dev/null || true
 }
 
-trap cleanup EXIT
+trap cleanup EXIT TERM INT
 
-emulator -avd "$AVD_NAME" -no-snapshot -no-window -ports ${CONSOLE_PORT},${ADBD_PORT} 2>&1 &
+emulator -avd "$AVD_NAME" $AUDIO_FLAG $CPU_FLAG $MEMORY_FLAG -no-snapshot -no-window -ports ${CONSOLE_PORT},${ADBD_PORT} 2>&1 &
 EMULATOR_PID=$!
 
 until nc -z 127.0.0.1 $ADBD_PORT 2>/dev/null; do
