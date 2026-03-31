73360af234074509b98ceb9ac7267a766a252b31
Spike Burton <sburton@macstadium.com>
2026-03-31 15:34:39 -0400

    add arg parsing

--- roles/install_orka_android_tools/files/run-avd.sh
+++ roles/install_orka_android_tools/files/run-avd.sh
@@ -1,17 +1,54 @@
 #!/bin/bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
-AVD_NAME="${1:-}"
-VM_BRIDGE_IP="${2:-192.168.64.1}"
-CONSOLE_PORT="${3:-5554}"
-ADBD_PORT=$((CONSOLE_PORT + 1))
-RELAY_PORT=$((ADBD_PORT + 10000))
+CONSOLE_PORT="5554"
+VMNET_BRIDGE_IP="192.168.64.1"
+VERBOSE=0
 
-if [[ -z "$AVD_NAME" ]]; then
-  echo "AVD name is required!"
+RELAY_PORT=""
+
+declare -a ARGS
+
+usage () {
+  echo "Usage: $0 AVD_NAME -b <VMNET_BRIDGE_IP> -p <CONSOLE_PORT> -r <RELAY_PORT>"
   exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+  unset OPTIND
+  unset OPTARG
+  while getopts "b:p:r:hv" opt; do
+    case $opt in
+      b) VMNET_BRIDGE_IP="$OPTARG" ;;
+      p) CONSOLE_PORT="$OPTARG" ;;
+      r) RELAY_PORT="$OPTARG" ;;
+      h) usage ;;
+      v) VERBOSE=1 ;;
+    esac
+  done
+  shift $(($OPTIND - 1))
+
+  if [[ -n "${1:-}" ]]; then
+    ARGS+=($1)
+    shift
+  fi
+done
+
+if [[ ${#ARGS[@]} -ne 1 ]]; then
+  echo "Error: AVD name is required!" >&2
+  usage
 fi
+
+if [[ "$VERBOSE" -eq 1 ]]; then
+  set -x
+fi
+
+AVD_NAME="${ARGS[0]}"
+ADBD_PORT="$((CONSOLE_PORT + 1))"
+DEFAULT_RELAY_PORT="$((ADBD_PORT + 10000))"
+
+RELAY_PORT="${RELAY_PORT:-$DEFAULT_RELAY_PORT}"
 
 EMULATOR_PID=""
 RELAY_PID=""
@@ -35,10 +72,8 @@ until nc -z 127.0.0.1 $ADBD_PORT 2>/dev/null; do
   sleep 2
 done
 
-socat TCP-LISTEN:${RELAY_PORT},fork,bind=${VM_BRIDGE_IP} TCP:127.0.0.1:${ADBD_PORT} 2>&1 &
+socat TCP-LISTEN:${RELAY_PORT},fork,bind=${VMNET_BRIDGE_IP} TCP:127.0.0.1:${ADBD_PORT} 2>&1 &
 RELAY_PID=$!
-
-set +x
 
 while kill -0 $EMULATOR_PID 2>/dev/null && kill -0 $RELAY_PID 2>/dev/null; do
   sleep 5
