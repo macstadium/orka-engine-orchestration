ceaaf160b9aef77f6472f718cd4e589dc3b3cc79
Spike Burton <sburton@macstadium.com>
2026-04-02 11:38:22 -0400

    add module documentation and examples

--- library/orka_avd_run.py
+++ library/orka_avd_run.py
@@ -1,6 +1,92 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+DOCUMENTATION = r"""
+---
+module: orka_avd_run
+short_description: Run an AVD (Android virtual device) on the host with connectivity to an Orka VM
+description:
+    - Runs an Android virtual device on the host using the Android SDK tools
+    - Starts a socat relay between the emulator ADBD port and the vmnet bridge interface for the Orka VM network
+options:
+    name:
+        description: Name of the Android virtual device
+        required: true
+        type: str
+    cpu:
+        description: Number of CPU cores to allocate
+        required: false
+        type: int
+    memory:
+        description: Amount of memory (in MB) to allocate
+        required: false
+        type: int
+    audio:
+        description: Enable audio for the Android virtual device
+        required: false
+        type: bool
+        default: false
+    bridge_ip: The IP address of the vmnet bridge interface for the Orka VM network
+        description:
+        required: false
+        type: str
+        default: "192.168.64.1"
+    android_home_path: Home path for the Android SDK
+        description:
+        required: false
+        type: str
+        default: "/opt/android-sdk"
+    run_avd_path: Path for the run-avd script
+        description:
+        required: false
+        type: str
+        default: "/opt/orka/bin/run-avd"
+    log_path: Path for the directory to store logs for the run-avd script
+        description:
+        required: false
+        type: str
+        default: "/opt/orka/logs/avd"
+author:
+    - "Spike Burton (@spikeburton)"
+"""
+
+EXAMPLES = r"""
+- name: Run AVD with specific CPU and memory requests
+  orka_avd_run:
+    name: "foo-avd-0"
+    cpu: 4
+    memory: 2048
+
+- name: Run AVD with audio enabled
+  orka_avd_run:
+    name: "foo-avd-0"
+    audio: true
+
+- name: Run AVD with specific vmnet bridge interface IP
+  orka_avd_run:
+    name: "foo-avd-0"
+    bridge_ip: 192.168.65.1
+"""
+
+RETURN = r"""
+changed:
+    description: Whether the task resulted in a change
+    type: bool
+    returned: always
+process_id:
+    description: Process ID of the run-avd script
+    type: int
+    returned: on success
+relay_ip:
+    description: The IP address that the AVD is accessible from within the Orka VM guest
+    type: str
+    returned: on success
+relay_port:
+    description: The port that the AVD is accessible from within the Orka VM guest
+    type: int
+    returned: on success
+"""
+
 import os
 import subprocess
 from ansible.module_utils.basic import AnsibleModule
